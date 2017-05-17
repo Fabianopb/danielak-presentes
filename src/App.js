a8c09c026aab7c5325d2353d5cb652495ba48734
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-17 11:25:16 -0300

    Requests API for testing response.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
+
+import Request from './modules/requests';
 import './App.css';
 
 injectTapEventPlugin();
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  componentWillMount() {
+    Request.getTest().then(response => this.setState({data: response.data}));
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <Paper className="app">
-          Test auto deploy
+          Test auto deploy - { this.state.data }
         </Paper>
       </MuiThemeProvider>
     );
