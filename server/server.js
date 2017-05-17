d5e9cd2908e24eaab49afc8a082f07d14257c477
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-17 10:35:10 -0300

    Revert "Removes static and cross origin handling from server."
    This reverts commit 34956c724df52dfcd82feab803adefd07b2aaa5a.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 // var mongoose = require('mongoose');
 // var users = require('./routes/users');
 // var beers = require('./routes/beers');
@@ -7,10 +8,22 @@ var app = express();
 
 // app.use(passport.initialize());
 
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   next();
+// });
+
 // app.use('/user', users);
 // app.use('/beers', beers);
 
 // mongoose.connect(process.env.BEER_CELLAR_MONGODB);
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
 
 app.get('/api', function(request, response) {
   response.json('We\'re up and running!');
