1369ca84e95485f670820b5870b37b310d74cba5
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-17 11:21:02 -0300

    Tests Material-UI Element.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 import './App.css';
+
+injectTapEventPlugin();
 
 class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        Test auto deploy
-      </div>
+      <MuiThemeProvider>
+        <Paper className="app">
+          Test auto deploy
+        </Paper>
+      </MuiThemeProvider>
     );
   }
 }
