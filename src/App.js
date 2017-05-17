008b96c94ba2c5ef7c4e002cacbdc0fee258e206
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-17 14:08:56 -0300

    Removes unused test code.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Paper from 'material-ui/Paper';
 
-import Request from './modules/requests';
 import './App.css';
 
 injectTapEventPlugin();
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null
-    }
-  }
-
-  componentWillMount() {
-    Request.getTest().then(response => this.setState({data: response.data}));
-  }
-
   render() {
     return (
       <MuiThemeProvider>
-        <Paper className="app">
-          Test auto deploy - { this.state.data }
-        </Paper>
+        <div>Test</div>
       </MuiThemeProvider>
     );
   }
