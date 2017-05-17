34956c724df52dfcd82feab803adefd07b2aaa5a
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-17 09:40:45 -0300

    Removes static and cross origin handling from server.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 // var mongoose = require('mongoose');
 // var users = require('./routes/users');
 // var beers = require('./routes/beers');
@@ -8,22 +7,10 @@ var path = require('path');
 
 // app.use(passport.initialize());
 
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   next();
-// });
-
 // app.use('/user', users);
 // app.use('/beers', beers);
 
 // mongoose.connect(process.env.BEER_CELLAR_MONGODB);
-
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-// });
 
 app.get('/api', function(request, response) {
   response.json('We\'re up and running!');
