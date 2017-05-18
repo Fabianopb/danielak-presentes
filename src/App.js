9e51feea3230448589d814e2456cd3ef7850f816
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-18 15:10:05 -0300

    Installs and configures routing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import ProductsView from './views/products/products';
 import AdminView from './views/admin/admin';
-import './app.css';
+import './App.css';
 
 injectTapEventPlugin();
 
@@ -13,7 +14,12 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <AdminView />
+        <Router>
+          <div>
+            <Route exact path="/" component={ ProductsView } />
+            <Route path="/admin" component={ AdminView } />
+          </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
