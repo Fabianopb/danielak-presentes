ccdb3a9611fb13c160c0f7d95fcecae78e5f5461
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-18 13:46:02 -0300

    Passes specific produc info to dialogs.

--- src/views/products/products.js
+++ src/views/products/products.js
@@ -10,23 +10,25 @@ class ProductsView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
-      open: false
+      products: null,
+      activeProduct: null,
+      isDialogOpen: false
     };
     this._handleOpen = this._handleOpen.bind(this);
     this._handleClose = this._handleClose.bind(this);
   }
 
   componentWillMount() {
-    this.setState({ data: productData.map(product => <div key={ product.id } className="product-cell" onTouchTap={ this._handleOpen }>{ product.name }</div>) });
+    this.setState({ products: productData.map(product => <div key={ product.id } className="product-cell" onTouchTap={ () => this._handleOpen(product) }>{ product.name }</div>) });
   }
 
-  _handleOpen() {
-    this.setState({ open: true });
+  _handleOpen(product) {
+    this.setState({ activeProduct: product });
+    this.setState({ isDialogOpen: true });
   };
 
   _handleClose() {
-    this.setState({ open: false });
+    this.setState({ isDialogOpen: false });
   };
 
   render() {
@@ -51,20 +53,20 @@ class ProductsView extends Component {
           DaniK
         </div>
         <div className="product-grid">
-          { this.state.data }
+          { this.state.products }
         </div>
         <div className="contact-footer">
           <p>danielalpresentes@yahoo.com.br</p>
           <p>Whatsapp +55 11 99777 5245</p>
         </div>
         <Dialog
-          title="Dialog With Actions"
+          title={ this.state.activeProduct && this.state.activeProduct.name }
           actions={ actions }
           modal={ false }
-          open={ this.state.open }
+          open={ this.state.isDialogOpen }
           onRequestClose={ this._handleClose }
         >
-          The actions in this window were passed in as an array of React objects.
+          { this.state.activeProduct && this.state.activeProduct.description }
         </Dialog>
       </div>
     );
