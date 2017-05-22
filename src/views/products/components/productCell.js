c8faeb1caa3c286d061d488c37d57df057e51aea
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-22 16:00:38 -0300

    Product dialog opens only when clicking in the picture.

--- src/views/products/components/productCell.js
+++ src/views/products/components/productCell.js
@@ -5,8 +5,8 @@ class ProductCell extends Component {
   render() {
     const product = this.props.product;
     return (
-      <div className="product-cell" onTouchTap={ this.props.onTouchTap }>
-        <div className="image"></div>
+      <div className="product-cell">
+        <div className="image" onTouchTap={ this.props.onTouchTap }></div>
         <div className="title">{ product.name }</div>
         <div className="current-price">{ product.currentPrice }</div>
       </div>
