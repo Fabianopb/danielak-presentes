554ac7c337439072c2186f6261bfd602fb6bb4f5
Fabiano Brito <fabianopbrito@gmail.com>
2017-06-27 15:02:20 +0300

    Installs redux and react-redux and creates basic redux setup.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {applyMiddleware, compose, createStore, combineReducers, bindActionCreators} from 'redux';
+import {Provider, connect} from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
+
+import App from './App';
 import './index.css';
 
+const initialState = {
+  test: 0
+};
+
+const middleware = [ /* add SagaMiddleware here */ ];
+const enhancers = [];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const testActionCreator = () => {
+  return {
+    type: 'TEST_ACTION'
+  };
+};
+
+// Create Reducers and pass them to the App's store
+
+const testReducer = (test = initialState.test, action = {}) => {
+  if (action.type === 'TEST_ACTION') {
+    console.log('woot', test);
+    return test + 1;
+  }
+  return test;
+};
+
+const reducers = combineReducers({
+  test: testReducer
+});
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(...middleware), ...enhancers)
+);
+
+// Create ConnectedApp which maps state and actions to the App's props
+
+const mapStateToProps = (state) => {
+  return {
+    test: state.test
+  };
+};
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({testActionCreator}, dispatch);
+
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
   document.getElementById('root')
 );
