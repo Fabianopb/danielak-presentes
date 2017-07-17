10d7a9f582431a00cceb2ca3a07f1069f5f8b04d
Fabiano Brito <fabianopbrito@gmail.com>
2017-07-17 09:46:31 +0300

    Defines data validation for product model.

--- server/models/product.js
+++ server/models/product.js
@@ -2,21 +2,67 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: String,
-  image: [String],
-  storeLink: String,
-  description: String,
-  currentPrice: Number,
-  discountPrice: Number,
-  tags: String,
-  productionTime: Number,
-  minAmount: Number,
-  width: Number,
-  height: Number,
-  depth: Number,
-  weight: Number,
-  isVisible: Boolean,
-  isFeatured: Boolean
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: [String],
+    required: true
+  },
+  storeLink: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  currentPrice: {
+    type: Number,
+    required: true
+  },
+  discountPrice: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  tags: {
+    type: String,
+    required: true
+  },
+  productionTime: {
+    type: Number,
+    required: true
+  },
+  minAmount: {
+    type: Number,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+  depth: {
+    type: Number,
+    required: true
+  },
+  weight: {
+    type: Number,
+    required: true
+  },
+  isVisible: {
+    type: Boolean,
+    required: true
+  },
+  isFeatured: {
+    type: Boolean,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
