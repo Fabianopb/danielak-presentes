48b3be0b3d129413d485d5891c6ee66abbd1eb64
Fabiano Brito <fabianopbrito@gmail.com>
2018-01-28 15:03:42 +0200

    Uses Semantic's Grid for the product grid.

--- src/containers/ProductsView.js
+++ src/containers/ProductsView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Grid, Dimmer, Loader } from 'semantic-ui-react';
 import { fetchProducts, showProductDetails } from '../modules/actions/products';
 
 import '../styles/products.css';
@@ -16,24 +16,26 @@ class ProductsView extends Component {
     const {isFetching, data} = this.props.products;
     return (
       <div className='products-view'>
-        <div className='product-grid'>
+        <Grid className='products-grid' columns={3} padded>
           {isFetching ? (
             <Dimmer active inverted>
               <Loader />
             </Dimmer>
           ) : data.map(product => (
-            <div className='product-cell' key={product._id}>
+            <Grid.Column className='product-cell' key={product._id}>
               <img
                 className='image'
                 src={product.image[product.featuredImageIndex]}
                 alt='N/A'
                 onClick={() => this.props.showProductDetails(product)}
               />
-              <div className='title'>{product.name}</div>
-              <div className='current-price'>{product.currentPrice}</div>
-            </div>
+              <div className='details'>
+                <div className='title'>{product.name}</div>
+                <div className='current-price'>{product.currentPrice}</div>
+              </div>
+            </Grid.Column>
           ))}
-        </div>
+        </Grid>
       </div>
     );
   }
