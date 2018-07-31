e80f831d59bd48a7440718eaf828c805dfcdaa6f
Fabiano Brito <fabianopbrito@gmail.com>
2018-07-31 21:18:43 +0300

    Relayout menu items.

--- src/containers/CategoryMenu/CategoryMenu.jsx
+++ src/containers/CategoryMenu/CategoryMenu.jsx
@@ -24,19 +24,22 @@ class CategoryMenu extends Component {
     return (
       <div className={styles.menu}>
         <div className={styles.itemsWrapper}>
-          <div
-            className={classNames(styles.menuItem, { [styles.activeItem]: !activeCategory })}
-            onClick={() => this.handleCategoryChange(null)}
-          >Todas
-          </div>
-          {_.map(data, category =>
+          <div className={styles.categories}>
             <div
-              key={category._id}
-              className={classNames(styles.menuItem, { [styles.activeItem]: activeCategory === category._id })}
-              onClick={() => this.handleCategoryChange(category._id)}
-            >{category.name}
+              className={classNames(styles.menuItem, { [styles.activeItem]: !activeCategory })}
+              onClick={() => this.handleCategoryChange(null)}
+            >Home
             </div>
-          )}
+            {_.map(data, category =>
+              <div
+                key={category._id}
+                className={classNames(styles.menuItem, { [styles.activeItem]: activeCategory === category._id })}
+                onClick={() => this.handleCategoryChange(category._id)}
+              >{category.name}
+              </div>
+            )}
+          </div>
+          {/*<div className={styles.menuItem}>Sobre</div>*/}
         </div>
       </div>
     );
