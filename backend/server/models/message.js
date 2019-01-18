eb03d6d1a9c6bc0c1d2039b12ae08b010669264d
Fabiano Brito <fabianopbrito@gmail.com>
2019-01-18 18:54:41 -0200

    Create message model and route with post method.

---
+++ backend/server/models/message.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  message: {
+    type: [String],
+    required: true,
+    default: null
+  },
+  new: {
+    type: Boolean,
+    default: true
+  },
+  answered: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
--- backend/server/server.js
+++ backend/server/server.js
@@ -9,6 +9,7 @@ const files = require('./routes/files');
 const products = require('./routes/products');
 const categories = require('./routes/categories');
 const users = require('./routes/users');
+const messages = require('./routes/messages');
 const passport = require('passport');
 
 app.use(passport.initialize());
@@ -17,6 +18,7 @@ app.use('/api/files', files);
 app.use('/api/products', products);
 app.use('/api/categories', categories);
 app.use('/api/users', users);
+app.use('/api/messages', messages);
 
 mongoose.connect(process.env.DANIK_MONGODB, { useNewUrlParser: true });
 
