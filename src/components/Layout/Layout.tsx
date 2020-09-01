8dcdf52658daeeebd97075039f801ea385955295
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-01 22:47:04 +0300

    Refactor Layout to be functional

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import danikLogo from '../../assets/danik-logo.png';
 import danikLua from '../../assets/danik-lua.png';
 import styles from './Layout.module.scss';
 
-class Layout extends React.Component<{}> {
-  public render() {
-    return (
-      <Grid fluid={true} className={styles.layout}>
-        <Row center="xs" className={styles.header}>
-          <Col xs={12} lg={8} className={styles.logoContainer}>
-            <img className={styles.logo} src={danikLogo} alt="logo" />
-            <img className={styles.lua} src={danikLua} alt="lua" />
-          </Col>
-        </Row>
-        <Row className={styles.content}>
-          <Col xs={12}>
-            {this.props.children}
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} className={styles.footer}>
-            <div>danielakpresentes@yahoo.com.br</div>
-            <div>Whatsapp +55 11 99777 5245</div>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+type LayoutProps = {
+  children: ReactNode;
 }
+
+const Layout = ({ children }: LayoutProps) => (
+  <Grid fluid={true} className={styles.layout}>
+    <Row center="xs" className={styles.header}>
+      <Col xs={12} lg={8} className={styles.logoContainer}>
+        <img className={styles.logo} src={danikLogo} alt="logo" />
+        <img className={styles.lua} src={danikLua} alt="lua" />
+      </Col>
+    </Row>
+    <Row className={styles.content}>
+      <Col xs={12}>
+        {children}
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12} className={styles.footer}>
+        <div>danielakpresentes@yahoo.com.br</div>
+        <div>Whatsapp +55 11 99777 5245</div>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 export default Layout;
