64fcd43341daf1c5339f43faa20e4334673ed84c
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-02 14:26:43 +0300

    Refactor AdminMain to be functional

--- src/containers/AdminProduct/AdminProduct.tsx
+++ src/containers/AdminProduct/AdminProduct.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { formValueSelector } from 'redux-form';
 import { Dimmer, Loader, Icon, Modal, Button, Header } from 'semantic-ui-react';
 import ProductForm from '../../forms/Product/Product';
-import { routerActions } from 'connected-react-router';
-import { productActions } from '../../actions/products';
-import { categoryActions } from '../../actions/categories';
+import { routerActions as cRouterActions } from 'connected-react-router';
+import { productActions as cProductActions } from '../../actions/products';
+import { categoryActions as cCategoryActions } from '../../actions/categories';
 import styles from './AdminProduct.module.scss';
 
 interface StateProps {
@@ -19,84 +19,84 @@ interface StateProps {
 }
 
 interface DispatchProps {
-  productActions: typeof productActions;
-  categoryActions: typeof categoryActions;
-  routerActions: typeof routerActions;
+  productActions: typeof cProductActions;
+  categoryActions: typeof cCategoryActions;
+  routerActions: typeof cRouterActions;
 }
 
 type AdminProductProps = StateProps & DispatchProps;
 
-class AdminProduct extends React.Component<AdminProductProps> {
-  public componentWillMount() {
-    this.props.categoryActions.fetchCategories();
-    this.props.productActions.fetchProducts(this.props.match);
-  }
+const AdminProduct = ({ 
+  products, 
+  categories, 
+  formValues,
+  match,
+  productActions,
+  categoryActions,
+  routerActions
+}: AdminProductProps) => {
+  useEffect(() => {
+    categoryActions.fetchCategories();
+    productActions.fetchProducts(match);
+  }, []);
 
-  public render() {
-    const { isFetching: isFetchingProducts, activeProduct, isDialogOpen } = this.props.products;
-    const { isFetching: isFetchingCategories, data: categories } = this.props.categories;
-    const { images } = this.props.formValues;
-    const { openDialog, closeDialog, deleteProduct, handleFileDrop, deleteImage } = this.props.productActions;
-    const { goBack } = this.props.routerActions;
-    const { match } = this.props;
-    return (
-      <div className={styles.adminProduct}>
-        <div className={styles.addProductHeader}>
-          <h3>Adicionar produto</h3>
-          <div className={styles.actionButtons}>
-            <Button basic={true} icon={true} labelPosition="right" color="blue" onClick={goBack}>
-              Voltar<Icon name="chevron left" />
-            </Button>
-            <Button
-              icon={true}
-              labelPosition="right"
-              color="red"
-              disabled={match === 'new'}
-              onClick={() => openDialog(activeProduct as Product)}
-            >
-              Remover
-              <Icon name="trash" />
-            </Button>
-          </div>
-        </div>
-        {isFetchingProducts || isFetchingCategories ? (
-          <Dimmer active={true} inverted={true}>
-            <Loader />
-          </Dimmer>
-        ) : (
-          <ProductForm
-            images={images}
-            handleFileDrop={handleFileDrop}
-            deleteImage={deleteImage}
-            categories={categories}
-            onSubmit={this.submitProduct}
-          />
-        )}
-        <Modal open={isDialogOpen} onClose={closeDialog} size="small">
-          <Header icon="trash" content="Apagar produto" />
-          <Modal.Content>
-            <p>Tem certeza que deseja apagar o produto <em>{activeProduct && activeProduct.name}</em>?</p>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button basic={true} icon={true} labelPosition="right" color="blue" onClick={closeDialog} >
-              Cancelar<Icon name="ban" />
-            </Button>
-            <Button icon={true} labelPosition="right" color="red" onClick={() => deleteProduct((activeProduct as Product)._id)} >
-              Remover<Icon name="remove" />
-            </Button>
-          </Modal.Actions>
-        </Modal>
-      </div>
-    );
-  }
-
-  private submitProduct = (product: Product): void => {
-    if (this.props.match === 'new') {
+  const submitProduct = (product: Product): void => {
+    if (match === 'new') {
       delete product._id;
     }
-    this.props.productActions.upsertProduct(product as Product);
+    productActions.upsertProduct(product as Product);
   }
-}
+
+  return (
+    <div className={styles.adminProduct}>
+      <div className={styles.addProductHeader}>
+        <h3>Adicionar produto</h3>
+        <div className={styles.actionButtons}>
+          <Button basic={true} icon={true} labelPosition="right" color="blue" onClick={routerActions.goBack}>
+            Voltar<Icon name="chevron left" />
+          </Button>
+          <Button
+            icon={true}
+            labelPosition="right"
+            color="red"
+            disabled={match === 'new'}
+            onClick={() => productActions.openDialog(products.activeProduct as Product)}
+          >
+            Remover
+            <Icon name="trash" />
+          </Button>
+        </div>
+      </div>
+      {products.isFetching || categories.isFetching ? (
+        <Dimmer active={true} inverted={true}>
+          <Loader />
+        </Dimmer>
+      ) : (
+        <ProductForm
+          images={formValues.images}
+          handleFileDrop={productActions.handleFileDrop}
+          deleteImage={productActions.deleteImage}
+          categories={categories.data}
+          onSubmit={submitProduct}
+        />
+      )}
+      <Modal open={products.isDialogOpen} onClose={productActions.closeDialog} size="small">
+        <Header icon="trash" content="Apagar produto" />
+        <Modal.Content>
+          <p>Tem certeza que deseja apagar o produto <em>{products.activeProduct && products.activeProduct.name}</em>?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic={true} icon={true} labelPosition="right" color="blue" onClick={productActions.closeDialog} >
+            Cancelar<Icon name="ban" />
+          </Button>
+          <Button icon={true} labelPosition="right" color="red" onClick={() => productActions.deleteProduct((products.activeProduct as Product)._id)} >
+            Remover<Icon name="remove" />
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: RootState) => ({
   products: state.products,
@@ -108,9 +108,9 @@ const mapStateToProps = (state: RootState) => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  productActions: bindActionCreators({ ...productActions }, dispatch),
-  categoryActions: bindActionCreators({ ...categoryActions }, dispatch),
-  routerActions: bindActionCreators({ ...routerActions }, dispatch),
+  productActions: bindActionCreators({ ...cProductActions }, dispatch),
+  categoryActions: bindActionCreators({ ...cCategoryActions }, dispatch),
+  routerActions: bindActionCreators({ ...cRouterActions }, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminProduct);
