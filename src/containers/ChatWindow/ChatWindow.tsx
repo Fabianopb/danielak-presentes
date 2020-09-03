a538d4c9ba9bb52382a79f165b098479fe6dade0
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-03 09:47:27 +0300

    Refactor ChatWindow to be functional

--- src/containers/ChatWindow/ChatWindow.tsx
+++ src/containers/ChatWindow/ChatWindow.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import { messageActions } from '../../actions/messages';
+import { messageActions as cMessageActions } from '../../actions/messages';
 import { Icon, Image, Input, InputOnChangeData } from 'semantic-ui-react';
 import cn from 'classnames';
 import moment from 'moment';
@@ -14,131 +14,68 @@ interface StateProps {
 }
 
 interface DispatchProps {
-  messageActions: typeof messageActions;
+  messageActions: typeof cMessageActions;
 }
 
 type ChatWindowProps = StateProps & DispatchProps;
 
-interface ChatWindowState {
-  isOpen: boolean;
-  input: string;
-  chatHistory: ChatHistory[];
-}
-
-const initialState: ChatWindowState = {
-  isOpen: false,
-  input: '',
-  chatHistory: [
-    {
-      speaker: 'dani',
-      message: 'Olá, posso ajudar? Deixe sua mensagem e seu contato que retornarei em breve!',
-      step: 0,
-    },
-  ],
+const initialMessage = {
+  speaker: 'dani' as const,
+  message: 'Olá, posso ajudar? Deixe sua mensagem e seu contato que retornarei em breve!',
+  step: 0,
 };
 
-class ChatWindow extends React.Component<ChatWindowProps, ChatWindowState> {
-  public state = initialState;
+const ChatWindow = ({ messages, messageActions }: ChatWindowProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [input, setInput] = useState('');
+  const [chatHistory, setChatHistory] = useState<ChatHistory[]>([initialMessage]);
 
-  private scrollEl = React.createRef<HTMLDivElement>();
+  const scrollEl = useRef<HTMLDivElement>(null);
 
-  public componentDidMount() {
+  useEffect(() => {
     const timestamp = localStorage.getItem('chatWindowTimestamp');
     const now = moment();
     if (!timestamp || (timestamp && moment(timestamp).isBefore(now))) {
       now.add(1, 'week');
       localStorage.setItem('chatWindowTimestamp', now.toISOString());
-      this.setState({ isOpen: true });
+      setIsOpen(true);
     }
+  }, []);
+
+  const toggleWindow = (): void => {
+    setIsOpen(!isOpen);
   }
 
-  public render() {
-    const { isOpen, input } = this.state;
-    const iconName = isOpen ? 'close' : 'talk';
-    return (
-      <div className={styles.chatWindow}>
-        <Icon name={iconName} circular={true} inverted={true} color="teal" size="large" onClick={this.toggleWindow} />
-        <div className={cn(styles.chatBox, { [styles.visible]: isOpen })}>
-          <div className={styles.content}>
-            <div className={styles.topbar}>
-              <Icon name="close" link={true} onClick={this.toggleWindow} />
-            </div>
-            <div className={styles.body}>
-              {this.state.chatHistory.map((history, index) => {
-                if (history.speaker === 'dani') {
-                  return (
-                    <div key={index} className={styles.danikMsg}>
-                      <div className={styles.iconWrapper}>
-                        <Image src={robotAvatar} bordered={true} circular={true} />
-                      </div>
-                      <div className={styles.bullet}>{history.message}</div>
-                    </div>
-                  );
-                } else if (history.speaker === 'user') {
-                  return (
-                    <div key={index} className={styles.userMsg}>
-                      <div className={styles.bullet}>{history.message}</div>
-                      <div className={styles.iconWrapper}>
-                        <Icon name="user" circular={true} inverted={true} color="grey" />
-                      </div>
-                    </div>
-                  );
-                } else {
-                  return null;
-                }
-              })}
-              <div ref={this.scrollEl} />
-            </div>
-            <div className={styles.messageInput}>
-              <Input
-                value={input}
-                transparent={true}
-                placeholder="Digite aqui..."
-                onChange={(_, data: InputOnChangeData) => this.setState({ input: data.value })}
-                onKeyPress={this.handleKeyPress}
-              />
-              <Icon name="send" color="grey" link={true} onClick={this.sendMessage} />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  private toggleWindow = (): void => {
-    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-  }
-
-  private handleKeyPress = (event: { key: string }) => {
+  const handleKeyPress = (event: { key: string }) => {
     if (event.key === 'Enter') {
-      this.sendMessage();
+      sendMessage();
     }
   }
 
-  private sendMessage = () => {
-    const { input, chatHistory } = this.state;
+  const sendMessage = () => {
     if (input) {
-      const newHistory = chatHistory.concat({ speaker: 'user', message: input });
-      this.setState({ input: '', chatHistory: newHistory });
-      this.setState({ input: '', chatHistory: newHistory }, () => this.scrollToBottom());
-      this.props.messageActions.saveMessage(newHistory, this.props.messages.activeMessageId);
-      delay(() => this.answerUser(), 2000);
+      const newEntry = chatHistory.concat({ speaker: 'user', message: input })
+      setInput('');
+      setChatHistory(newEntry);
+      scrollToBottom();
+      messageActions.saveMessage(newEntry, messages.activeMessageId);
+      delay(answerUser, 2000);
     }
   }
 
-  private answerUser = () => {
-    const { chatHistory } = this.state;
+  const answerUser = () => {
     const lastDaniMsg = findLast(chatHistory, history => history.speaker === 'dani') as ChatHistory;
     if (lastDaniMsg.step === 0) {
-      const newHistory = chatHistory.concat({
+      const newEntry = chatHistory.concat({
         speaker: 'dani',
         message: 'Não esqueça de deixar seu nome, telefone/whatsapp ou e-mail! :)',
         step: 1,
       });
-      this.setState({ chatHistory: newHistory }, () => this.scrollToBottom());
+      setChatHistory(newEntry);
+      scrollToBottom();
     }
     if (lastDaniMsg.step === 1) {
-      const newHistory = chatHistory.concat([
+      const newEntry = chatHistory.concat([
         {
           speaker: 'dani',
           message: 'Obrigada! Pode continuar enviando mensagens se desejar.',
@@ -150,23 +87,75 @@ class ChatWindow extends React.Component<ChatWindowProps, ChatWindowState> {
           step: 2,
         },
       ]);
-      this.setState({ chatHistory: newHistory }, () => this.scrollToBottom());
+      setChatHistory(newEntry);
+      scrollToBottom();
     }
   }
 
-  private scrollToBottom = () => {
-    if (this.scrollEl && this.scrollEl.current) {
-      this.scrollEl.current.scrollIntoView({ behavior: 'smooth' });
+  const scrollToBottom = () => {
+    if (scrollEl.current) {
+      scrollEl.current.scrollIntoView({ behavior: 'smooth' });
     }
   }
-}
+
+  const iconName = isOpen ? 'close' : 'talk';
+
+  return (
+    <div className={styles.chatWindow}>
+      <Icon name={iconName} circular={true} inverted={true} color="teal" size="large" onClick={toggleWindow} />
+      <div className={cn(styles.chatBox, { [styles.visible]: isOpen })}>
+        <div className={styles.content}>
+          <div className={styles.topbar}>
+            <Icon name="close" link={true} onClick={toggleWindow} />
+          </div>
+          <div className={styles.body}>
+            {chatHistory.map((history, index) => {
+              if (history.speaker === 'dani') {
+                return (
+                  <div key={index} className={styles.danikMsg}>
+                    <div className={styles.iconWrapper}>
+                      <Image src={robotAvatar} bordered={true} circular={true} />
+                    </div>
+                    <div className={styles.bullet}>{history.message}</div>
+                  </div>
+                );
+              } else if (history.speaker === 'user') {
+                return (
+                  <div key={index} className={styles.userMsg}>
+                    <div className={styles.bullet}>{history.message}</div>
+                    <div className={styles.iconWrapper}>
+                      <Icon name="user" circular={true} inverted={true} color="grey" />
+                    </div>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })}
+            <div ref={scrollEl} />
+          </div>
+          <div className={styles.messageInput}>
+            <Input
+              value={input}
+              transparent={true}
+              placeholder="Digite aqui..."
+              onChange={(_, data: InputOnChangeData) => setInput(data.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <Icon name="send" color="grey" link={true} onClick={sendMessage} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: RootState) => ({
   messages: state.messages,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  messageActions: bindActionCreators({ ...messageActions }, dispatch),
+  messageActions: bindActionCreators({ ...cMessageActions }, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatWindow);
