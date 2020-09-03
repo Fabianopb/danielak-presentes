9dbfa0fafce239173a55bcdbef5f35198010d4f2
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-03 09:53:12 +0300

    Refactor LoginPage to be functional

--- src/containers/LoginPage/LoginPage.tsx
+++ src/containers/LoginPage/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { Dimmer, Loader } from 'semantic-ui-react';
-import { userActions } from '../../actions/users';
+import { userActions as cUserActions } from '../../actions/users';
 import LoginForm from '../../forms/Login/Login';
 import styles from './LoginPage.module.scss';
 
@@ -11,41 +11,34 @@ interface StateProps {
 }
 
 interface DispatchProps {
-  userActions: typeof userActions;
+  userActions: typeof cUserActions;
 }
 
 type LoginPageProps = StateProps & DispatchProps;
 
-class LoginPage extends React.Component<LoginPageProps> {
-  public render() {
-    const { isLogging } = this.props.users;
-    return (
-      <div className={styles.loginPage}>
-        <h3 className={styles.loginTitle}>Login</h3>
-        {isLogging ? (
-          <Dimmer active={true} inverted={true}>
-            <Loader />
-          </Dimmer>
-        ) : (
-          <div className={styles.loginContainer}>
-            <LoginForm onSubmit={this.handleSubmit} />
-          </div>
-        )}
+const LoginPage = ({ users, userActions }: LoginPageProps) => (
+  <div className={styles.loginPage}>
+    <h3 className={styles.loginTitle}>Login</h3>
+    {users.isLogging ? (
+      <Dimmer active={true} inverted={true}>
+        <Loader />
+      </Dimmer>
+    ) : (
+      <div className={styles.loginContainer}>
+        <LoginForm onSubmit={creds => {
+          userActions.login(creds);
+        }} />
       </div>
-    );
-  }
-
-  private handleSubmit = (credentials: LoginRequestParams): void => {
-    this.props.userActions.login(credentials);
-  }
-}
+    )}
+  </div>
+);
 
 const mapStateToProps = (state: RootState) => ({
   users: state.users,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  userActions: bindActionCreators({ ...userActions }, dispatch),
+  userActions: bindActionCreators({ ...cUserActions }, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
