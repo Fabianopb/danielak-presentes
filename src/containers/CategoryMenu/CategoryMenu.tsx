7a5cf0c7df590fbc1119c7bf91cce4ee70e784bb
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-03 09:33:39 +0300

    Refactor CategoryMenu to be functional

--- src/containers/CategoryMenu/CategoryMenu.tsx
+++ src/containers/CategoryMenu/CategoryMenu.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
-import { RouterState, routerActions } from 'connected-react-router';
+import { RouterState, routerActions as cRouterActions } from 'connected-react-router';
 import { connect } from 'react-redux';
 import { Row, Col } from 'react-flexbox-grid';
 import classNames from 'classnames';
 import _ from 'lodash';
 import queryString from 'query-string';
-import { productActions } from '../../actions/products';
-import { categoryActions } from '../../actions/categories';
-import { userActions } from '../../actions/users';
+import { productActions as cProductActions } from '../../actions/products';
+import { categoryActions as cCategoryActions } from '../../actions/categories';
+import { userActions as cUserActions } from '../../actions/users';
 import { isAdminPage } from '../../modules/helpers';
 import styles from './CategoryMenu.module.scss';
 
@@ -18,66 +18,66 @@ interface StateProps {
 }
 
 interface DispatchProps {
-  categoryActions: typeof categoryActions;
-  productActions: typeof productActions;
-  routerActions: typeof routerActions;
-  userActions: typeof userActions;
+  categoryActions: typeof cCategoryActions;
+  productActions: typeof cProductActions;
+  routerActions: typeof cRouterActions;
+  userActions: typeof cUserActions;
 }
 
 type CategoryMenuProps = StateProps & DispatchProps;
 
-class CategoryMenu extends React.Component<CategoryMenuProps> {
-  public componentDidMount() {
-    this.props.categoryActions.fetchCategories();
-  }
+const CategoryMenu = ({
+  categories,
+  router,
+  categoryActions,
+  productActions,
+  routerActions,
+  userActions,
+}: CategoryMenuProps) => {
+  useEffect(() => {
+    categoryActions.fetchCategories()
+  }, []);
 
-  public componentDidUpdate(prevProps: CategoryMenuProps) {
-    if (!_.isEqual(prevProps.router.location.search, this.props.router.location.search)) {
-      this.props.productActions.fetchProducts();
+  useEffect(() => {
+    if (router.location.search) {
+      productActions.fetchProducts();
     }
-  }
+  }, [router.location.search]);
 
-  public render() {
-    const { data } = this.props.categories;
-    const { pathname } = this.props.router.location;
-    const { logout } = this.props.userActions;
-    const { push } = this.props.routerActions;
-    const { search } = this.props.router.location;
-    const query = queryString.parse(search);
-    const isRoot = pathname === '/';
-    return (
-      <Row center="xs" className={styles.menu}>
-        <Col xs={12} lg={8} >
-          <div className={styles.itemsWrapper}>
-            {!isAdminPage(pathname) &&
-              <React.Fragment>
-                <div className={styles.categories}>
-                  <div
-                    className={classNames(styles.menuItem, { [styles.activeItem]: isRoot && !query.category })}
-                    onClick={() => push(`/`)}
-                  >Home
-                  </div>
-                  {_.map(data, (category, index) =>
-                    <div
-                      key={index}
-                      className={classNames(styles.menuItem, { [styles.activeItem]: isRoot && query.category === category._id })}
-                      onClick={() => push(`/?category=${category._id}`)}
-                    >{category.name}
-                    </div>,
-                  )}
+  const query = queryString.parse(router.location.search);
+  const isRoot = router.location.pathname === '/';
+  return (
+    <Row center="xs" className={styles.menu}>
+      <Col xs={12} lg={8} >
+        <div className={styles.itemsWrapper}>
+          {!isAdminPage(router.location.pathname) &&
+            <React.Fragment>
+              <div className={styles.categories}>
+                <div
+                  className={classNames(styles.menuItem, { [styles.activeItem]: isRoot && !query.category })}
+                  onClick={() => routerActions.push(`/`)}
+                >Home
                 </div>
-                <div className={styles.menuItem} onClick={() => push('/about')}>Contato</div>
-              </React.Fragment>
-            }
-            {isAdminPage(pathname) &&
-              <div className={styles.menuItem} onClick={logout}>Logout</div>
-            }
-          </div>
-        </Col>
-      </Row>
-    );
-  }
-}
+                {_.map(categories.data, (category, index) =>
+                  <div
+                    key={index}
+                    className={classNames(styles.menuItem, { [styles.activeItem]: isRoot && query.category === category._id })}
+                    onClick={() => routerActions.push(`/?category=${category._id}`)}
+                  >{category.name}
+                  </div>,
+                )}
+              </div>
+              <div className={styles.menuItem} onClick={() => routerActions.push('/about')}>Contato</div>
+            </React.Fragment>
+          }
+          {isAdminPage(router.location.pathname) &&
+            <div className={styles.menuItem} onClick={userActions.logout}>Logout</div>
+          }
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 const mapStateToProps = (state: RootState) => ({
   categories: state.categories,
@@ -85,10 +85,10 @@ const mapStateToProps = (state: RootState) => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  categoryActions: bindActionCreators({ ...categoryActions }, dispatch),
-  productActions: bindActionCreators({ ...productActions }, dispatch),
-  routerActions: bindActionCreators({ ...routerActions }, dispatch),
-  userActions: bindActionCreators({ ...userActions }, dispatch),
+  categoryActions: bindActionCreators({ ...cCategoryActions }, dispatch),
+  productActions: bindActionCreators({ ...cProductActions }, dispatch),
+  routerActions: bindActionCreators({ ...cRouterActions }, dispatch),
+  userActions: bindActionCreators({ ...cUserActions }, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CategoryMenu);
