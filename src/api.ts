e98f1c51c21d0994fb51c0d459c31fdb43b88261
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-07 22:39:10 +0300

    Add call to fetch categories

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
 export const fetchAllProducts = () => axios.get<Product[]>(`/api/products`).then(res => res.data);
+
+export const fetchCategories = () => axios.get<Category[]>(`/api/categories`).then(res => res.data);
