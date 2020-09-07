d76dd0d8cb0d8fcb5576103f54570a05b01a1ca8
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-07 22:56:20 +0300

    Use query params to manage category id

--- src/containers/NewCategoryMenu/NewCategoryMenu.tsx
+++ src/containers/NewCategoryMenu/NewCategoryMenu.tsx
@@ -1,104 +1,67 @@
-import React, { useEffect } from 'react';
-import { bindActionCreators, Dispatch } from 'redux';
-import { RouterState, routerActions as cRouterActions } from 'connected-react-router';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import classNames from 'classnames';
-import _ from 'lodash';
-import queryString from 'query-string';
-import { productActions as cProductActions } from '../../actions/products';
-import { categoryActions as cCategoryActions } from '../../actions/categories';
-import { userActions as cUserActions } from '../../actions/users';
+import useSWR from 'swr';
+// import { useHistory } from 'react-router-dom';
+import { useQueryParam, StringParam } from 'use-query-params';
 import { isAdminPage } from '../../modules/helpers';
 import styles from './NewCategoryMenu.module.scss';
+import { fetchCategories } from '../../api';
 
-interface StateProps {
-  categories: CategoriesState;
-  router: RouterState;
-}
+const NewCategoryMenu = () => {
+  const { data: categories } = useSWR('/categories', fetchCategories);
 
-interface DispatchProps {
-  categoryActions: typeof cCategoryActions;
-  productActions: typeof cProductActions;
-  routerActions: typeof cRouterActions;
-  userActions: typeof cUserActions;
-}
+  const [categoryId, setCategoryId] = useQueryParam('categoryId', StringParam);
 
-type NewCategoryMenuProps = StateProps & DispatchProps;
+  // useEffect(() => {
+  //   if (router.location.search) {
+  //     productActions.fetchProducts();
+  //   }
+  // }, [router.location.search, productActions]);
 
-const NewCategoryMenu = ({
-  categories,
-  router,
-  categoryActions,
-  productActions,
-  routerActions,
-  userActions,
-}: NewCategoryMenuProps) => {
-  useEffect(() => {
-    categoryActions.fetchCategories();
-  }, [categoryActions]);
-
-  useEffect(() => {
-    if (router.location.search) {
-      productActions.fetchProducts();
-    }
-  }, [router.location.search, productActions]);
-
-  const query = queryString.parse(router.location.search);
-  const isRoot = router.location.pathname === '/';
+  const isRoot = window.location.pathname === '/';
   return (
     <Row center="xs" className={styles.menu}>
       <Col xs={12} lg={8}>
         <div className={styles.itemsWrapper}>
-          {!isAdminPage(router.location.pathname) && (
+          {!isAdminPage(window.location.pathname) && (
             <>
               <div className={styles.categories}>
                 <div
                   className={classNames(styles.menuItem, {
-                    [styles.activeItem]: isRoot && !query.category,
+                    [styles.activeItem]: isRoot && !categoryId,
                   })}
-                  onClick={() => routerActions.push(`/`)}
+                  onClick={() => setCategoryId(undefined)}
                 >
                   Home
                 </div>
-                {_.map(categories.data, (category, index) => (
-                  <div
-                    key={index}
-                    className={classNames(styles.menuItem, {
-                      [styles.activeItem]: isRoot && query.category === category._id,
-                    })}
-                    onClick={() => routerActions.push(`/?category=${category._id}`)}
-                  >
-                    {category.name}
-                  </div>
-                ))}
+                {categories &&
+                  categories.map(category => (
+                    <div
+                      key={category._id}
+                      className={classNames(styles.menuItem, {
+                        [styles.activeItem]: isRoot && categoryId === category._id,
+                      })}
+                      onClick={() => setCategoryId(category._id)}
+                    >
+                      {category.name}
+                    </div>
+                  ))}
               </div>
-              <div className={styles.menuItem} onClick={() => routerActions.push('/about')}>
+              {/* <div className={styles.menuItem} onClick={() => routerActions.push('/about')}>
                 Contato
-              </div>
+              </div> */}
             </>
           )}
-          {isAdminPage(router.location.pathname) && (
+          {/* {isAdminPage(window.location.pathname) && (
             <div className={styles.menuItem} onClick={userActions.logout}>
               Logout
             </div>
-          )}
+          )} */}
         </div>
       </Col>
     </Row>
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  categories: state.categories,
-  router: state.router,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  categoryActions: bindActionCreators({ ...cCategoryActions }, dispatch),
-  productActions: bindActionCreators({ ...cProductActions }, dispatch),
-  routerActions: bindActionCreators({ ...cRouterActions }, dispatch),
-  userActions: bindActionCreators({ ...cUserActions }, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewCategoryMenu);
+export default NewCategoryMenu;
