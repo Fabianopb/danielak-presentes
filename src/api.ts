f4b14f73135c7c3f88c2973aeb423600a3175779
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-08 22:07:19 +0300

    Add methods to create and edit messages

--- src/api.ts
+++ src/api.ts
@@ -11,3 +11,11 @@ export const fetchProductById = (productId: string) =>
   axios.get<Product[]>(`/api/products?_id=${productId}`).then(res => res.data[0]);
 
 export const fetchCategories = () => axios.get<Category[]>(`/api/categories`).then(res => res.data);
+
+export const createMessage = (text: string[]) =>
+  axios
+    .post<{ id: string }>('/api/messages', { text })
+    .then(res => res.data);
+
+export const editMessage = (id: string, text: string[]) =>
+  axios.put(`/api/messages/${id}`, { text });
