9b320f9e65c7bda96e849806f27fb38c4e9c6cbd
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-08 19:50:35 +0300

    Add method to get product by id

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const fetchAllProducts = () => axios.get<Product[]>(`/api/products`).then(res => res.data);
 
+export const fetchProductById = (productId: string) =>
+  axios.get<Product[]>(`/api/products?_id=${productId}`).then(res => res.data[0]);
+
 export const fetchCategories = () => axios.get<Category[]>(`/api/categories`).then(res => res.data);
