c9f3f9927985a301babc691afe5fd2aaddecb863
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-08 22:26:08 +0300

    Add methods to toggle message visibility and to delete message

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const getAuthHeaders = () => ({
+  Authorization: `Bearer ${localStorage.getItem('token')}`,
+});
+
 export const loginAdminUser = (credentials: { email: string; password: string }) =>
   axios
     .post<{ token: string; expiry: string }>(`/api/users/login`, credentials)
@@ -12,10 +16,18 @@ export const fetchProductById = (productId: string) =>
 
 export const fetchCategories = () => axios.get<Category[]>(`/api/categories`).then(res => res.data);
 
+export const fetchMessages = () => axios.get<Message[]>(`/api/messages`).then(res => res.data);
+
+export const toggleMessageVisibility = (messageId: string) =>
+  axios.put(`/api/messages/answer/${messageId}`, {}, { headers: getAuthHeaders() });
+
 export const createMessage = (text: string[]) =>
   axios
     .post<{ id: string }>('/api/messages', { text })
     .then(res => res.data);
 
-export const editMessage = (id: string, text: string[]) =>
-  axios.put(`/api/messages/${id}`, { text });
+export const editMessage = (messageId: string, text: string[]) =>
+  axios.put(`/api/messages/${messageId}`, { text });
+
+export const deleteMessage = (messageId: string) =>
+  axios.delete(`/api/messages/${messageId}`, { headers: getAuthHeaders() });
