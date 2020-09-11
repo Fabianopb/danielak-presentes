5fef33d860594f0e0c816dd4a4ece67828bbb4c7
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-12 01:10:41 +0300

    Use uuid v4 with example table id

--- pg-server/migrations/20200910212240_hello_world_table.ts
+++ pg-server/migrations/20200910212240_hello_world_table.ts
@@ -1,8 +1,9 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   return knex.schema.createTable('hello_world', table => {
-    table.uuid('id').primary();
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
     table.string('name');
     table.integer('age');
   });
