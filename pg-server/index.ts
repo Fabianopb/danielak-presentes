cd5115594db54e15c9deff070f5ccb3840600256
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-11 09:22:32 +0300

    Set db and log running port

--- pg-server/index.ts
+++ pg-server/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import Knex from 'knex';
+import knexfile from './knexfile';
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.get('/api/v2', (req, res) => {
   res.send('Hello PG world!');
 });
 
+app.set('db', Knex(knexfile.development));
 app.listen(9001);
 // eslint-disable-next-line no-console
-console.log('PG server up and running...');
+console.log(`PG server up and running in ${process.env.NODE_ENV} on :9001...`);
