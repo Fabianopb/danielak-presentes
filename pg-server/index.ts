b9488743fa1fdd9dc13ab166c978b2f15c422948
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-12 01:18:11 +0300

    List all entries of the db as example

--- pg-server/index.ts
+++ pg-server/index.ts
@@ -11,6 +11,12 @@ app.get('/api/v2', (req, res) => {
   res.send('Hello PG world!');
 });
 
+app.get('/api/v2/example', async (req, res) => {
+  const db = await Knex(knexfile.development);
+  const result = await db('hello_world').select();
+  return res.json(result);
+});
+
 app.set('db', Knex(knexfile.development));
 app.listen(9001);
 // eslint-disable-next-line no-console
