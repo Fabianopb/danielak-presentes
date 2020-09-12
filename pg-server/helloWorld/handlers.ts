e5e2774b9092e20fe6a67a65c3042b63af83725b
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-12 19:23:20 +0300

    Use config to get db connection

--- pg-server/helloWorld/handlers.ts
+++ pg-server/helloWorld/handlers.ts
@@ -1,8 +1,7 @@
-import Knex from 'knex';
-import knexfile from '../knexfile';
 import { Example } from './types';
+import { getDb } from '../config';
 
 export const selectAllExamples = async () => {
-  const db = await Knex(knexfile.development);
+  const db = await getDb();
   return db<Example>('hello_world').select();
 };
