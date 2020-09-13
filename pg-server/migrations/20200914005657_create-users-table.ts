2f75aebd2972e5d84bfe58a77c4499b3300e321f
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-14 01:10:38 +0300

    Add users table migration

---
+++ pg-server/migrations/20200914005657_create-users-table.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+  return knex.schema.createTable('users', table => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+    table.string('email');
+    table.string('salt');
+    table.string('hash');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('users');
+}
