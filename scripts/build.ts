4d6b1c67bc022075fe0c170ec736ee54a931c56f
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-13 17:54:41 +0300

    Add migration to postbuild script

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,3 +2,4 @@ import cp from 'child_process';
 
 cp.spawn('yarn', ['build-server'], { stdio: 'inherit' });
 cp.spawn('yarn', ['build-client'], { stdio: 'inherit' });
+cp.spawn('yarn', ['migrate:latest'], { stdio: 'inherit' });
