9214aee096fe99fb924c2958c349d94b477a9bf5
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-15 21:53:51 +0300

    Add create categories table migration

--- pg-server/migrations/20200914010105_create-categories-table.ts
+++ pg-server/migrations/20200914010105_create-categories-table.ts
@@ -3,9 +3,10 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('categories', table => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name');
-    table.string('description');
-    table.boolean('removed');
+    table.string('name').notNullable();
+    table.text('description').notNullable();
+    table.boolean('removed').defaultTo(false).notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
   });
 }
 
