01c44d9bba0e15d7b17a48210f57736a4093a36b
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-15 21:53:59 +0300

    Add create products table migration

--- pg-server/migrations/20200914010304_create-products-table.ts
+++ pg-server/migrations/20200914010304_create-products-table.ts
@@ -3,23 +3,24 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('products', table => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name');
-    table.integer('featuredImageIndex');
+    table.string('name').notNullable();
+    table.integer('featuredImageIndex').defaultTo(0).notNullable();
     table.string('storeLink');
-    table.string('description');
-    table.uuid('categoryId');
-    table.integer('currentPrice');
+    table.text('description').defaultTo('').notNullable();
+    table.uuid('categoryId').notNullable();
+    table.integer('currentPrice').notNullable();
     table.integer('discountPrice');
-    table.jsonb('tags');
-    table.integer('productionTime');
-    table.integer('minAmount');
-    table.integer('width');
-    table.integer('height');
-    table.integer('depth');
-    table.integer('weight');
-    table.boolean('isVisible');
-    table.boolean('isFeatured');
-    table.jsonb('images');
+    table.jsonb('tags').defaultTo([]).notNullable();
+    table.integer('productionTime').notNullable();
+    table.integer('minAmount').notNullable();
+    table.integer('width').notNullable();
+    table.integer('height').notNullable();
+    table.integer('depth').notNullable();
+    table.integer('weight').notNullable();
+    table.boolean('isVisible').defaultTo(true).notNullable();
+    table.boolean('isFeatured').defaultTo(false).notNullable();
+    table.jsonb('images').defaultTo([]).notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
   });
 }
 
