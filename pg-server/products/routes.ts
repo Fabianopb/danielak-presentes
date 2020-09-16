e44ffd4e1978d04f2b599faa661ade87fabda355
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-16 21:56:50 +0300

    Create product routes

---
+++ pg-server/products/routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+import authorize from '../auth/authorize';
+import { selectAllProducts, insertProduct, updateProduct, deleteProduct } from './handlers';
+
+const router = Router();
+
+router.get('/products', async (req, res) => {
+  const products = await selectAllProducts();
+  return res.status(200).json(products);
+});
+
+router.post('/products', authorize, bodyParser.json(), async (req, res) => {
+  await insertProduct(req.body);
+  return res.status(200).json({ message: 'New product saved!' });
+});
+
+router.put('/products/:id', authorize, bodyParser.json(), async (req, res) => {
+  await updateProduct(req.params.id, req.body);
+  return res.status(200).json({ message: 'Product updated' });
+});
+
+router.delete('/products/:id', authorize, async (req, res) => {
+  await deleteProduct(req.params.id);
+  return res.status(200).json({ message: 'Product removed' });
+});
+
+export default router;
