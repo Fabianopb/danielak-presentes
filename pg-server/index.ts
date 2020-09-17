364bad13b779ed5e632dc4220d6dda517f182f9c
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-18 00:34:26 +0300

    Make pg-server the main one

--- pg-server/index.ts
+++ pg-server/index.ts
@@ -1,14 +1,33 @@
+/// <reference types="./declarations" />
 import express from 'express';
 import path from 'path';
+import passport from 'passport';
+import userV2Routes from './users/routes';
+import categoriesV2Routes from './categories/routes';
+import messagesV2Routes from './messages/routes';
+import productsV2Routes from './products/routes';
+import filesV2Routes from './files/routes';
 
 const app = express();
 
+app.use(passport.initialize());
+
+app.use('/api/v2', [
+  userV2Routes,
+  categoriesV2Routes,
+  messagesV2Routes,
+  productsV2Routes,
+  filesV2Routes,
+]);
+
 app.use(express.static(path.resolve('build')));
 
-app.get('/api/v2', (req, res) => {
-  res.send('Hello PG world!');
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve('build', 'index.html'));
 });
 
-app.listen(9001);
+const port = process.env.PORT || 9000;
+
+app.listen(port);
 // eslint-disable-next-line no-console
-console.log(`PG server up and running in ${process.env.NODE_ENV} on :9001...`);
+console.log(`Server up and running on :${port}`);
