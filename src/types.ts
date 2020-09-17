216dd4a8a02a37d70b3ac54339051905a56d8f3d
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-17 23:26:15 +0300

    Remove MongoProduct type

--- src/types.ts
+++ src/types.ts
@@ -4,27 +4,6 @@ export type ClientImage = {
   loading?: boolean;
 };
 
-export type MongoProduct = {
-  _id: string;
-  name: string;
-  image: ClientImage[];
-  featuredImageIndex: number;
-  storeLink: string;
-  description: string;
-  category: string;
-  currentPrice: number;
-  discountPrice: number;
-  tags: string;
-  productionTime: number;
-  minAmount: number;
-  width: number;
-  height: number;
-  depth: number;
-  weight: number;
-  isVisible: boolean;
-  isFeatured: boolean;
-};
-
 export type MongoCategory = {
   _id?: string;
   name: string;
