8301d04e978a48ab1a261f246a1dc6ea2edef75b
Fabiano Brito <fabiano.brito@wolt.com>
2020-09-21 20:51:55 +0300

    Wrap user routes and handle unauthorized custom error

--- pg-server/users/routes.ts
+++ pg-server/users/routes.ts
@@ -4,23 +4,39 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import { registerAdminUser, generateJwt } from './handlers';
 import initPassport from '../auth/passport';
-import { User } from '../types';
+import { asyncHandler } from '../utils';
+
+class UnauthorizedError extends Error {
+  public statusCode?: number;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = 401;
+  }
+}
 
 initPassport();
 const router = Router();
 
-router.route('/users/register').get(async (req, res) => {
-  await registerAdminUser();
-  return res.status(200).json('Admin user registered!');
-});
+router.route('/users/register').get(
+  asyncHandler(async (req, res) => {
+    await registerAdminUser();
+    return res.status(200).json('Admin user registered!');
+  }),
+);
 
-router.route('/users/login').post(bodyParser.json(), passport.authenticate('local'), (req, res) => {
-  const user = req.user as User | undefined;
-  if (!user) {
-    return res.status(401).json('info');
-  }
-  const tokenSignature = generateJwt(user.id, user.email);
-  return res.status(200).json(tokenSignature);
+router.route('/users/login').post(bodyParser.json(), (req, res, next) => {
+  passport.authenticate('local', (error, user, info) => {
+    if (error) {
+      next(error);
+    }
+    if (!user) {
+      throw new UnauthorizedError(info.message);
+    }
+    const tokenSignature = generateJwt(user.id, user.email);
+    return res.status(200).json(tokenSignature);
+  })(req, res, next);
 });
 
 export default router;
