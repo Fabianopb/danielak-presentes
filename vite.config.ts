032ee5b2a5e3062f7c5ccbc550df581127892b6b
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-13 23:30:36 +0300

    Add vite configs and modernize tsconfig

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const port = process.env.PORT || 9090;
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-styled-components', { displayName: true, fileName: false }]],
+      },
+    }),
+  ],
+  server: {
+    open: true,
+    proxy: {
+      '^/api/.*': `http://localhost:${port}`,
+    },
+  },
+  define: {
+    global: {},
+  },
+});
