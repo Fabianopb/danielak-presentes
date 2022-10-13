7227ee07d8e2ea1d461bfe4b1f80d9efad61b7e6
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-13 23:54:39 +0300

    Run migration from heroku postbuild script

--- scripts/build.ts
+++
@@ -1,5 +0,0 @@
-import cp from 'child_process';
-
-cp.spawn('yarn', ['build-server'], { stdio: 'inherit' });
-cp.spawn('yarn', ['build-client'], { stdio: 'inherit' });
-cp.spawn('yarn', ['migrate:latest'], { stdio: 'inherit' });
