4bb69f849c12ea50c06dbd8762e9e3f7c7a40d1d
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-14 00:11:37 +0300

    Spawn sync child processes for building

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,7 @@
 import cp from 'child_process';
 
-cp.spawn('yarn', ['build-client'], { stdio: 'inherit' });
-cp.spawn('yarn', ['build-server'], { stdio: 'inherit' });
-cp.spawn('yarn', ['migrate:latest'], { stdio: 'inherit' });
+(() => {
+  cp.spawnSync('yarn', ['build-client'], { stdio: 'inherit' });
+  cp.spawnSync('yarn', ['build-server'], { stdio: 'inherit' });
+  cp.spawnSync('yarn', ['migrate:latest'], { stdio: 'inherit' });
+})();
