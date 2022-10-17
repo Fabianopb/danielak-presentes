e22e68a3e2e70e3d74028edc5fa1fc40ce32ef76
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-17 22:08:42 +0300

    Create mongodb instance

---
+++ pg-server/database.ts
@@ -0,0 +1,43 @@
+import { MongoClient, Db } from 'mongodb';
+import {} from './collections';
+import {} from './schemas';
+
+const cloudServer = process.env.APP_ENV !== 'production' ? '' : '+srv';
+const user = encodeURIComponent(process.env.DANIK_MONGO_USERNAME || '');
+const password = encodeURIComponent(process.env.DANIK_MONGO_PASSWORD || '');
+const cluster = process.env.DANIK_MONGO_CLUSTER;
+const uri = `mongodb${cloudServer}://${user}:${password}@${cluster}/?retryWrites=true&w=majority`;
+
+const databaseName = process.env.DANIK_MONGO_DB_NAME;
+let client: MongoClient;
+export let database: Db;
+
+// const createSchemaSetter =
+//   (existingCollectionNames: string[]) => async (collectionName: string, validator: Document) => {
+//     if (existingCollectionNames.includes(collectionName)) {
+//       await database.command({ collMod: collectionName, validator });
+//     } else {
+//       await database.createCollection(collectionName, { validator });
+//     }
+//   };
+
+export const init = async () => {
+  if (!uri) {
+    throw new Error('MONGO_URI is not defined in the environment!');
+  }
+  if (!databaseName) {
+    throw new Error('MONGO_DB_NAME is not defined in the environment!');
+  }
+  client = new MongoClient(uri);
+
+  await client.connect();
+  database = client.db(databaseName);
+
+  // const collections = await database.collections();
+  // const existingCollectionNames = collections.map((c) => c.collectionName);
+  // const setupSchema = createSchemaSetter(existingCollectionNames);
+};
+
+export const close = () => {
+  client.close();
+};
