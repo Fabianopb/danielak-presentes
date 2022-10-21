7ba8d07a3f3415ee681c49f2fb2e90422e3c57f7
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-21 20:09:44 +0300

    Remove deprecated scripts

--- scripts/build.ts
+++
@@ -1,7 +0,0 @@
-import cp from 'child_process';
-
-(() => {
-  cp.spawnSync('yarn', ['build-client'], { stdio: 'inherit' });
-  cp.spawnSync('yarn', ['build-server'], { stdio: 'inherit' });
-  cp.spawnSync('yarn', ['migrate:latest'], { stdio: 'inherit' });
-})();
