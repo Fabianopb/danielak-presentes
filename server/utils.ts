8ca169d5498f23586e1201653aab67a325205a13
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-21 20:13:49 +0300

    Finalize clean up of the server

--- server/utils.ts
+++ server/utils.ts
@@ -1,5 +1,3 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
-
 export class BadRequestError extends Error {
   statusCode = 400;
   name = 'BadRequestError';
@@ -19,13 +17,3 @@ export class NotFoundError extends Error {
   statusCode = 404;
   name = 'NotFoundError';
 }
-
-export const serializePayload = <T extends Record<string, unknown>>(payload: T) =>
-  Object.entries(payload).reduce(
-    (acc, [key, value]) =>
-      typeof value === 'object' && value !== null ? { ...acc, [key]: JSON.stringify(value) } : { ...acc, [key]: value },
-    {}
-  );
-
-export const asyncHandler = (fn: RequestHandler) => (req: Request, res: Response, next: NextFunction) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
