b17f83113704a4ab45e034179d0d917eed001020
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-21 19:24:05 +0300

    Remove asyncHandler from files routes

--- pg-server/routes/files.ts
+++ pg-server/routes/files.ts
@@ -4,7 +4,6 @@ import FileType from 'file-type';
 import fs from 'fs';
 import { Router } from 'express';
 import multiparty from 'multiparty';
-import { asyncHandler } from '../utils';
 import auth from '../auth';
 
 AWS.config.update({
@@ -71,32 +70,28 @@ export const deleteImageFiles = (imageUrls: string[]) => {
 
 const router = Router();
 
-router.post(
-  '/files/upload',
-  auth,
-  asyncHandler(async (req, res) => {
-    const form = new multiparty.Form();
-    form.parse(req, async (error, fields, files) => {
-      if (error) {
-        return res.status(500).send(error);
-      }
-      try {
-        const data = await processAndUploadFile(files.file[0].path);
-        return res.status(200).send(data);
-      } catch (err) {
-        return res.status(400).send(err);
-      }
-    });
-  })
-);
+router.post('/files/upload', auth, async (req, res, next) => {
+  const form = new multiparty.Form();
+  form.parse(req, async (error, fields, files) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    try {
+      const data = await processAndUploadFile(files.file[0].path);
+      return res.status(200).send(data);
+    } catch (error) {
+      next(error);
+    }
+  });
+});
 
-router.post(
-  '/files/delete',
-  auth,
-  asyncHandler(async (req, res) => {
+router.post('/files/delete', auth, async (req, res, next) => {
+  try {
     const data = await deleteImageFiles(req.body.images);
     return res.status(200).send(data);
-  })
-);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
