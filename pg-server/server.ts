0854a0646bde1a8f1938450d467b518c2e2d027e
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-21 19:52:59 +0300

    Switch server to use v3 product routes

--- pg-server/server.ts
+++ pg-server/server.ts
@@ -1,11 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
-import productsV2Routes from './products/routes';
 import filesRoutes from './routes/files';
 import bodyParser from 'body-parser';
 import categoriesRouter from './routes/categories';
 import usersRouter from './routes/users';
 import messagesRouter from './routes/messages';
+import productsRouter from './routes/products';
 
 const port = process.env.PORT || 9090;
 
@@ -15,9 +15,9 @@ export const init = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.use('/api/v2', [productsV2Routes, filesRoutes]);
+  app.use('/api/v2', [filesRoutes]);
 
-  app.use('/api/v3', [categoriesRouter, usersRouter, messagesRouter]);
+  app.use('/api/v3', [categoriesRouter, usersRouter, messagesRouter, productsRouter]);
 
   app.use(express.static(path.resolve('danik-dist')));
 
